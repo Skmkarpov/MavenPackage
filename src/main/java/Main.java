23058c90aecaa88027906e407906a447cfb78ab4
Владислав Маслов <maslov-vd.inc@msk.elara.ru>
2023-03-15 11:43:42 +0300

    Версия 1.0

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ import Netology.MavenPackage.CalcService;
 public class Main {
     public static void main(String[] args) {
         CalcService service = new CalcService();
-        int count = service.calc(1000, 10000);
+        int count = service.calc(5000, 10000);
         System.out.println(count);
     }
 }
